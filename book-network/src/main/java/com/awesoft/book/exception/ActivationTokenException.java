3c48a7162aa3f6519663423a3e67a695a99b18e6
riarana1 <riarana1@yahoo.com>
2025-10-03 16:10:17 -0500

    First commit book network

---
+++ book-network/src/main/java/com/awesoft/book/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.awesoft.book.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
